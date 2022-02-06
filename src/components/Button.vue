bdeccb98423e09c3335ae6986a9260139dba4a29
Umair Ahmed <umair.ahmad1257@gmail.com>
2022-02-06 19:17:51 +0500

    Initial

---
+++ src/components/Button.vue
@@ -0,0 +1,21 @@
+<template>
+  <button @click="onClick($event)" :style="{ background: color }" class="btn">
+    {{ text }}
+  </button>
+</template>
+<script>
+export default {
+  name: "Button",
+  props: {
+    text: String,
+    color: String,
+  },
+  methods: {
+    onClick() {
+      this.$emit("toggle-add-task");
+    },
+  },
+  emits: ["toggle-add-task"],
+};
+</script>
+<style></style>
---
+++ src/components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <Button
+      @toggle-add-task="$emit('toggle-add-task')"
+      :text="showAddTask ? 'Collapse' : 'Add a Task'"
+      :color="showAddTask ? 'red' : 'green'"
+    />
+  </header>
+</template>
+<script>
+import Button from "./Button.vue";
+export default {
+  name: "Header",
+  props: {
+    title: String,
+    showAddTask: Boolean,
+  },
+  components: {
+    Button,
+  },
+  emits: ["toggle-add-task"],
+};
+</script>
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
